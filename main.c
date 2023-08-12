9a5fa70dcea0c758d2037a93b1f8c3091745f978
EFRAIM DE JESUS FARIAS COSTA <efraim.costa@a.ucb.br>
2023-08-12 10:21:54 -0300

    parte da revisão C

---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+double calcular_dobro(double n){
+    return n * 2;
+}
+
+int main(int argc, char* argv[]){
+
+    printf("O dobro de %lf eh %lf\n", 20.3, calcular_dobro(20.3));
+
+    return 0;
+}
