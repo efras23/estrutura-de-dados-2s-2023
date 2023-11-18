cfd8602450a2fd9ca085fed004c163a9a36752c4
EFRAIM DE JESUS FARIAS COSTA <efraim.costa@a.ucb.br>
2023-11-18 10:50:45 -0300

    Árevore Binária

---
+++ arvore_binaria/arvore_binaria.h
@@ -0,0 +1,8 @@
+typedef struct No {
+    int valor;
+    struct No* no_esquerda;
+    struct No* no_direita;
+} No;
+
+void ab_inserir_no(No** raiz, int valor);
+void ab_imprimir_em_ordem(No* raiz);
---
+++ arvore_binaria/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "arvore_binaria.h"
+
+int main(int argc, char* argv[]){
+    No* raiz = NULL;
+    ab_inserir_no(&raiz, 2);
+
+    
+
+    return 0;
+}
